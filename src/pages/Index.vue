6002bbfa4b3cb034f615ac3a5f595462a3a20919
Eder Sena <eder.sena@live.com>
2021-11-28 18:03:41 -0300

    chart finalizado

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,6 +1,33 @@
 <template>
-  <q-page class="flex flex-center">
-    <h1>Ae</h1>
+  <q-page>
+    <div class="row q-ma-sm q-col-gutter-sm">
+      <div class="col-6">
+        <q-card>
+          <q-card-section>
+            <apexchart
+              height="350"
+              type="bar"
+              :options="options"
+              :series="series"
+            />
+          </q-card-section>
+        </q-card>
+      </div>
+
+      <div class="col-6">
+        <q-card>
+          <q-card-section>
+            <apexchart
+              height="350"
+              type="bar"
+              :options="options"
+              :series="series"
+            />
+          </q-card-section>
+        </q-card>
+      </div>
+  </div>
+
   </q-page>
 </template>
 
@@ -8,6 +35,22 @@
 import { defineComponent } from 'vue'
 
 export default defineComponent({
-  name: 'PageIndex'
+  name: 'PageIndex',
+  data () {
+    return {
+      options: {
+        chart: {
+          id: 'vuechart-example'
+        },
+        xaxis: {
+          categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998]
+        }
+      },
+      series: [{
+        name: 'series-1',
+        data: [30, 40, 45, 50, 49, 60, 70, 91]
+      }]
+    }
+  }
 })
 </script>
